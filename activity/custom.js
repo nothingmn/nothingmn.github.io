162fc62c690dee57ba22ddc6269c75aef61c423b
Rob Chartier <robc@moj.io>
2015-08-17 16:36:00 -0700

    added maps

--- activity/custom.js
+++ activity/custom.js
@@ -6,22 +6,32 @@ $(function() {
 	var template = $('#activity-template').html();
 	Mustache.parse(template);   // optional, speeds up future uses
 
-  var vehicleID = getParameterByName("vehicleId") || "423a8b56-ae89-424f-a0be-0afadc8cccb7";
-  var url = "http://mojiobroker1.cloudapp.net/activities/activity?&sort={%22Revision%22:-1}&query={%22$and%22:[{%22VehicleId%22:%22"+vehicleID+"%22}]}";
+  var vehicleID = getParameterByName("vehicleId") || "740c7492-c140-4574-aabf-21cd3e188f4e";
+  var url = "http://mojiobroker1.cloudapp.net/activities/activity?&sort={Revision:-1}&query={%22$and%22:[{%22VehicleId%22:%22"+vehicleID+"%22}]}&limit=50";
   console.log(vehicleID, url);
 
 	$.ajax({
 	  url: url,
 	  success: function(data,status,xhr) {
-		  	var data = data.reverse();
+		  	var last = "";
+		  	var list = [];
   	  	    for(var d in data) {
 		  	 	var item = data[d];
-				item.PublishedDate = new Date((item.Published[0] - 621355968000000000) / 10000);
-				item.humanPublishedDate = humaneDate(item.PublishedDate);
+		  	 	if(last == "") {
+		  	 		last = item.Title;
+		  	 	} else {
+		  	 		if(last != item.Title) {
+		  	 			list.push(item);
+		  	 			last = item.Title;
+		  	 		}
+		  	 	}
+				item.DeviceTime = new Date((item.DeviceTime[0] - 621355968000000000) / 10000);
+				item.humanPublishedDate = humaneDate(item.DeviceTime);
 				item.Image = "https://my.moj.io/vehicle/avatar/ebde9622-6a70-4608-9d06-4634bdb346c2?size=Large&imageCode=-8587639033764869069";
 				item.Name = "Robs Truck";
 		  	}
-		  	var binding = { data : data }
+		  	var list = _.sortBy( list, 'DeviceTime' ).reverse();
+		  	var binding = { data : list }
 		  	var rendered = Mustache.render(template, binding);
 		  	$('#activities').html(rendered);
 		  	console.log(binding);
